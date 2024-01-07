deac87a24c273b895d63f9e364abefd7a0b86bd9
Enoch Kibet <enochblake9@gmail.com>
2024-01-08 02:17:45 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+function manipulateDOM() {
+
+    const mainElement = document.getElementById('main');
+    if (mainElement) {
+      mainElement.remove();
+    }
+
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+  
+    newHeader.innerHTML = "YOUR-NAME is the champion";
+  
+    document.body.appendChild(newHeader);
+  
+    return newHeader;
+  }
+
+  const newHeader = manipulateDOM();
+ 
+  window.newHeader = newHeader;
+  
